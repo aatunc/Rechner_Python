3b13b0f10d160a294feb75c4b72e73bc4eb87b49
A Tunc <106016458+aatunc@users.noreply.github.com>
2023-03-23 12:21:47 +0100

    Initial commit

---
+++ .idea/Rechner.py
@@ -0,0 +1,14 @@
+# Rechner-Code in Python
+
+# Eingabeaufforderung für den Benutzer
+print("Willkommen beim Rechner!")
+print("Bitte geben Sie Ihre Berechnung ein.")
+
+# Eingabe der Berechnung
+berechnung = input("")
+
+# Auswerten der Berechnung
+ergebnis = eval(berechnung)
+
+# Ausgabe des Ergebnisses
+print("Das Ergebnis Ihrer Berechnung ist: ", ergebnis)
